cd4d43d8db68fd75d62cec3dcba95349ee477f2d
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-03-23 06:53:12 +0530

    commented in files so that i can understand afterwards

--- Notes-a/async.js
+++ Notes-a/async.js
@@ -1,16 +1,16 @@
 function add(x,y){
-    return x+y
+    return x+y                                       //    this function does instantly
 }
 function slowAdd(x,y){
-    let after4Sec=new Date().getTime()+4000
-    while(after4Sec>new Date()){}
-    return add(x,y)
+    let after4Sec=new Date().getTime()+4000         //    this function is taking 4sec but JS won't process anything 
+    while(after4Sec>new Date()){}                   //    other than this, it will wait 4sec and then give output
+    return add(x,y)                                 //    and then proceed further. Synchronously.
 }
 function sayHiSlowly() {
     let greeting='hiii'
-    setTimeout(function(){
-        console.log(greeting,', The wait is over')
-    },2000)
+    setTimeout(function(){                          //    this takes 2sec to say hiii, JS will do other work here
+        console.log(greeting,', The wait is over')  //    as it is creating an interrupt! which puts the callback
+    },2000)                                         //    into the queue after 2sec
     console.log('i finished working')
 }
 
@@ -20,3 +20,7 @@ console.log(slowAdd(3,33))
 sayHiSlowly()
 console.log(slowAdd(3,53))
 console.log(add(2,22))
+
+// hence waits for 4sec nothing happens 36 prints, then we call async sayHiSlowly() it puts a function in queue after
+// 2sec the next line 'i finsihed working prints before' then we have slowAdd(3,53) waiting for 4sec and then
+// immediately after that is add(2,22) prints 24, then the queued function get called and 'hiii' is printed.
--- Notes-a/async2.js
+++ Notes-a/async2.js
@@ -1,7 +1,7 @@
 const s = new Date().getSeconds();
 
 setTimeout(function() {
-  // prints out "2", meaning that the callback is not called immediately after 500 milliseconds.
+  // prints out "4", meaning that the callback is not called immediately after 500 milliseconds.
   console.log("Ran after " + (new Date().getSeconds() - s) + " seconds");
 }, 500);
 
@@ -11,3 +11,13 @@ while(true) {
     break;
   }
 }
+
+while(true) {
+  if(new Date().getSeconds() - s >= 8) {
+     console.log("Good, looped for 4 seconds again");
+    break;
+  }
+}
+
+// this example shows the queue thing clearly the setTimeout must have print after 500ms but it took 4sec to do so
+// as before that while loop was being executed. which takes 4sec so output came after 4sec.
